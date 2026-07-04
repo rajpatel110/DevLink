7612ebce1599b7f6a4e2d209145e2fc2546f9155
Raj patel <9795093341raj@gmail.com>
2026-07-04 11:51:26 +0530

    init-database

--- src/app.js
+++ src/app.js
@@ -1,9 +1,34 @@
 const express = require('express');
-require("./config/database");
+const connectDB =require("./config/database");
 
 const app = express();
+const User=require("./models/user");
 
+app.post("/signup",async(req,res)=>{
+    const user=new User({
+        firstName:"Raj",
+        lastName:"Patel",
+        emailId:"Rajpatel@123.com",
+        password:"raj@123",
+    });
+    try{
+        await user.save();
+        res.send("User added successfuly");
+    }catch(err){
+        res.status(400).send("error sving the user"+err.message);
+    }
+});
 
-app.listen(3000,()=>{
+connectDB()
+.then(()=>{
+    console.log("Database connection successful..");
+    app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 });
+}).catch(err=>{
+    console.log("Database cannot be connected..");
+})
+
+
+
+
--- src/config/database.js
+++ src/config/database.js
@@ -4,9 +4,5 @@ const connectDB = async ()=>{
     await mongoose.connect("mongodb+srv://341rajpatel_db_user:Rajpatel123@cluster0.nzegymb.mongodb.net/DevLink");
 }
 
-connectDB().then(()=>{
-    console.log("Database connection successful..");
-}).catch(err=>{
-    console.log("Database cannot be connected..");
-})
+module.exports = connectDB;
 
